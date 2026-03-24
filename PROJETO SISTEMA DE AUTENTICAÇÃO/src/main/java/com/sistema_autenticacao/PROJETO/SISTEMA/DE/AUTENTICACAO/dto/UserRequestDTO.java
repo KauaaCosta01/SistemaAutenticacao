52dcb89a990e56f218ddbe13c401b42942b91b45
Kaua Costa <rodrigokaua19@gmail.com>
2026-03-24 16:08:19 -0300

    Feat: Adicionar validação de DTO e manipulador de exceções global.

--- PROJETO SISTEMA DE AUTENTICAÇÃO/src/main/java/com/sistema_autenticacao/PROJETO/SISTEMA/DE/AUTENTICACAO/dto/UserRequestDTO.java
+++ PROJETO SISTEMA DE AUTENTICAÇÃO/src/main/java/com/sistema_autenticacao/PROJETO/SISTEMA/DE/AUTENTICACAO/dto/UserRequestDTO.java
@@ -1,5 +1,7 @@
 package com.sistema_autenticacao.PROJETO.SISTEMA.DE.AUTENTICACAO.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +9,15 @@ import lombok.Setter;
 @Setter
 public class UserRequestDTO {
 
+
+    @NotBlank(message = "Nome é Obrigatório")
     private String name;
+
+    @Email(message = "E-mail inválido")
+    @NotBlank(message = "E-mail é obrigatório")
     private String email;
+
+    @NotBlank(message = "Senha deve ter no mínimo 6 Dígitos")
     private String password;
 
 }
